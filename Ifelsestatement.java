9d83866273b4d9d9ade3dd4dfb64508e0efec34d
TharanyaG <55039539+TharanyaG@users.noreply.github.com>
2024-08-13 15:50:50 +1000

    Add files via upload

---
+++ Ifelsestatement.java
@@ -0,0 +1,15 @@
+package week1day2;
+
+public class Ifelsestatement {
+public static void main(String[] args) {
+	int m = 45;
+	if(m<20) {
+		System.out.println ("C");}
+	else if(m<40) {
+		System.out.println("B");}
+	else if(m<50) {
+		System.out.println("A");}
+	else {
+		System.out.println("O");}
+}
+}
